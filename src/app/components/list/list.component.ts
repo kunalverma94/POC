690d35f00bbd1acb22c2de3b071ab6acd72676fa
V-Kunal Verma <Kunal.Verma@blackbaud.me>
2020-08-09 02:36:21 +0530

    scroll

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SpaceShuttle } from 'src/app/models/SpaceShuttle';
 import { FilterService } from 'src/app/services/filter-service/filter.service';
@@ -9,10 +9,11 @@ import { SpaceXDataService } from 'src/app/services/space-x-data-service/space-x
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit, AfterViewInit {
+export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   //#region Properties
   public filterViewList: Observable<SpaceShuttle[]>;
   public loading = false;
+  public limit = 30;
   //#endregion
 
   constructor(private dataService: SpaceXDataService, private filterService: FilterService) {}
@@ -20,11 +21,13 @@ export class ListComponent implements OnInit, AfterViewInit {
   //#region LifeCycle Hooks
   ngOnInit(): void {
     this.filterViewList = this.dataService.getSpaceData();
+    addEventListener('scroll', this.loadEvent());
   }
 
   ngAfterViewInit(): void {
     this.filterService.AppliedFilters.subscribe(
       (fdd) => {
+        this.limit = 30;
         // Filter Mode and Filter Applied
         if (this.filterService.filterMode) {
           if (FilterService.hasFilter(fdd)) {
@@ -40,10 +43,21 @@ export class ListComponent implements OnInit, AfterViewInit {
       }
     );
   }
+
+  ngOnDestroy(): void {
+    removeEventListener('scroll', this.loadEvent());
+  }
   //#endregion
 
   //#region Methods
   public trackItem = (i, item) => item.flight_number;
 
+  private loadEvent(): (this: Window, ev: Event) => any {
+    return () => {
+      if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+        this.limit += 10;
+      }
+    };
+  }
   //#endregion
 }
