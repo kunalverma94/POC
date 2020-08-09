511acd9d18a69a41ddc56c8a79d506b909f74048
V-Kunal Verma <Kunal.Verma@blackbaud.me>
2020-08-09 13:03:34 +0530

    list update

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -12,31 +12,28 @@ import { SpaceXDataService } from 'src/app/services/space-x-data-service/space-x
 export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   //#region Properties
   public filterViewList: Observable<SpaceShuttle[]>;
-  public loading = false;
   public limit = 20;
-
   //#endregion
 
   constructor(private dataService: SpaceXDataService, private filterService: FilterService) {}
 
   //#region LifeCycle Hooks
   ngOnInit(): void {
-    this.filterViewList = this.dataService.getSpaceData();
+    this.loadList();
     window.addEventListener('scroll', this.loadEvent());
   }
 
   ngAfterViewInit(): void {
     this.filterService.AppliedFilters.subscribe(
       (fdd) => {
-        this.limit = 30;
+        this.limit = 20;
         // Filter Mode and Filter Applied
         if (this.filterService.filterMode) {
           if (FilterService.hasFilter(fdd)) {
             this.filterViewList = this.dataService.getSpaceData(fdd);
           }
         } else {
-          // normal loading
-          this.filterViewList = this.dataService.getSpaceData();
+          this.loadList();
         }
       },
       (error) => {
@@ -51,8 +48,10 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   //#endregion
 
   //#region Methods
+  // re-render selected elements:Performance
   public trackItem = (i, item) => item.flight_number;
 
+  // scroll event
   private loadEvent(): (this: Window, ev: Event) => any {
     const MOBILE_OFFSET = 150;
     return () => {
@@ -60,6 +59,10 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
         this.limit += 10;
       }
     };
+  }
+
+  private loadList() {
+    this.filterViewList = this.dataService.getSpaceData();
   }
   //#endregion
 }
