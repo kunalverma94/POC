a0ab05d07b55ae9d92c06ea9c3beebbad71910a1
V-Kunal Verma <Kunal.Verma@blackbaud.me>
2020-08-09 13:39:47 +0530

    fix-2

--- src/app/components/shared/not-found/not-found/not-found.component.ts
+++ src/app/components/shared/not-found/not-found/not-found.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  template: '<h1>404 Not found.</h1>',
+  template: '<h1>404 Not found.</h1> <br/> <a routerLink="/home">Go tohome</a>',
 })
 export class NotFoundComponent implements OnInit {
   constructor() {}
